16da5f3061299bfea7be57e11b3075c1b3c76a4c
juanWijaya0603 <juanw0603@gmail.com>
2024-11-20 20:08:55 +0700

    update : nambah page untuk nambahdata dan membuat xml untuk sebuah card detail pekerjaan di page beranda

--- app/src/main/java/app/c14210290/latihandaftarpekerjaan/nambahAtauEdit_Data.kt
+++ app/src/main/java/app/c14210290/latihandaftarpekerjaan/nambahAtauEdit_Data.kt
@@ -1,32 +1,20 @@
 package app.c14210290.latihandaftarpekerjaan
 
-import android.content.Intent
-import android.content.intent_Nambahpekerjaaan
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class nambahAtauEdit_Data : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_nambah_atau_edit_data)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
-        val nambahPekerjaan = findViewById<ImageView>(R.id.nambahPekerjaan)
-
-        nambahPekerjaan.setOnClickListener{
-            val intent_Nambahpekerjaaan = Intent(this@MainActivity,nambahAtauEdit_Data::class.java)
-            startActivity(intent_Nambahpekerjaaan)
-        }
-
     }
 }
